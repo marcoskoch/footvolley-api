ee598341c2287ef4074fe7f02e1ae63527bde81b
Marcos Koch <marcos_koch@hotmail.com>
2020-10-19 10:59:42 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import Brute from 'express-brute';
-import BruteRedis from 'express-brute-redis';
+// import Brute from 'express-brute';
+// import BruteRedis from 'express-brute-redis';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
@@ -25,17 +25,17 @@ import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 const upload = multer(multerConfig);
 
-const bruteStore = new BruteRedis({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-});
+// const bruteStore = new BruteRedis({
+//   host: process.env.REDIS_HOST,
+//   port: process.env.REDIS_PORT,
+// });
 
-const bruteForce = new Brute(bruteStore);
+// const bruteForce = new Brute(bruteStore);
 
 routes.post('/users', validateUserStore, UserController.store);
 routes.post(
   '/sessions',
-  bruteForce.prevent,
+  // bruteForce.prevent,
   validateSessionStore,
   SessionController.store
 );
