fc5cc434fb09a384f5d4276e051c87a04b6d8fd2
Marcos Koch <marcos_koch@hotmail.com>
2021-02-01 16:25:57 -0300

    Update ProviderController.js

--- src/app/controllers/ProviderController.js
+++ src/app/controllers/ProviderController.js
@@ -4,7 +4,7 @@ import File from '../models/File';
 class ProviderController {
   async index(req, res) {
     const providers = await User.findAll({
-      where: { provider: true, id: 4 },
+      where: { provider: true },
       attributes: ['id', 'name', 'email', 'avatar_id'],
       include: [
         {
